b92e76bdeac55dafaac2a37a89514088909cb497
Alex.Avila <alex.avila@sisudevices.com>
2023-03-17 16:05:21 -0500

    Refactor: Convert game component to a functional component

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useRef, useState} from 'react';
 import {calcRowLen,calculateTotalHolds,mapFromBoard, mapLastInRowFromBoard,dist} from "../Helper Functions/board calculator";
 import {shuffleArray, mergeObjects} from "../Helper Functions/Generic Helpers"
 import Hold from "./Hold.js";
@@ -8,87 +8,83 @@ import {Tile, limbType, groupType} from "./Tile.js";
 import {Limb} from "./Limb.js";
 import './Game.css';
 
-class Game extends React.Component{
-	constructor(props){
-		super(props);
-		const drawPile = this.generateDrawPile();
-		shuffleArray(drawPile, this.props.cardSeed);
-		this.state = {drawIndex: 0, 
-			drawPile: drawPile, 
-			displayDraw: true,
+function Game (props){
 
-			limbData: {leftHand: {coords: [2,2], isAtStart: false, selected: false, group: [groupType.left,groupType.hand]},
-				rightHand: {coords: [2,2], isAtStart: false, selected: false, group: [groupType.right,groupType.hand]},
-				leftFoot: {coords: [2,2], isAtStart: false, selected: false, group: [groupType.left,groupType.foot]},
-				rightFoot: {coords: [2,2], isAtStart: false, selected: false, group: [groupType.right,groupType.foot]},
-				weight: {coords: [2,2], isAtStart: false, selected: false, group:[]}},
-		}
-
-		this.generateMoveTree();
-
-		this.drawCards = this.drawCards.bind(this);
-		this.selectLimb = this.selectLimb.bind(this);
-		this.deselectLimb = this.deselectLimb.bind(this);
-		this.limbHandleClick = this.limbHandleClick.bind(this);
-		this.moveLimbs = this.moveLimbs.bind(this);
-		this.holdHandleClick = this.holdHandleClick.bind(this);
+	const generateDrawPile = () => {
+		const drawPile = colorsArray.map((firstColor,index) => {
+			return (colorsArray.slice(index, colorsArray.length).map(secondColor => {
+				return ({ID: uuid(), data: {colors: [firstColor, secondColor]}})
+			}))
+		}).flat();
+		return drawPile;
 	}
 
-// Object.values(limbType).map((type) => this.state.limbData[type])
+	const [drawIndex,setDrawIndex] = useState(0);
+	const [drawPile,setDrawPile] = useState(generateDrawPile()); //move to useRef
+	const [displayDraw,setDisplayDraw] = useState(true);
+	const [limbData,setLimbData] = useState(
+		{leftHand: {coords: [2,2], isAtStart: false, selected: false, group: [groupType.left,groupType.hand]},
+		rightHand: {coords: [2,2], isAtStart: false, selected: false, group: [groupType.right,groupType.hand]},
+		leftFoot: {coords: [2,2], isAtStart: false, selected: false, group: [groupType.left,groupType.foot]},
+		rightFoot: {coords: [2,2], isAtStart: false, selected: false, group: [groupType.right,groupType.foot]},
+		weight: {coords: [2,2], isAtStart: false, selected: false, group:[]}}
+		);
 
-	generateTilesData(nRows, nCols){
-		const holdData  = this.generateHoldData();
-		let tiles = this.mapHoldsToTiles(nRows, nCols, holdData);	
-		this.mapLimbsToTiles(tiles);
+// Object.values(limbType).map((type) => limbData[type])
+
+	const generateTilesData = (nRows, nCols) =>{
+		const holdData  = generateHoldData();
+		let tiles = mapHoldsToTiles(nRows, nCols, holdData);	
+		mapLimbsToTiles(tiles);
 		return tiles;
 	}
 
-	generateHoldData(){
+	const generateHoldData = () => {
 		let holdData = [];
-		const nTotalTiles =  calculateTotalHolds(this.props.nRows, this.props.nCols);
+		const nTotalTiles =  calculateTotalHolds(props.nRows, props.nCols);
 		const iterNum = Math.ceil(nTotalTiles/colorsArray.length);
 		for (var i = 0; i < iterNum; i++) {
 			holdData = holdData.concat(colorsArray.map((color) =>({color: color})));
 		}
-		shuffleArray(holdData,this.props.boardSeed);
+		shuffleArray(holdData,props.boardSeed);
 		holdData = holdData.slice(0,nTotalTiles);
 		return holdData;
 	}
 
-	mapHoldsToTiles(nRows, nCols, holdData){
+	const mapHoldsToTiles = (nRows, nCols, holdData) =>{
 		let tiles = [];
 		for (var i = 0; i < nRows; i++) {
 			tiles[i] = {ID: uuid(),
-				data: this.generateRowData(nCols, i, holdData.slice(mapFromBoard(i,0,nCols),mapLastInRowFromBoard(i,nCols)+1))};
+				data: generateRowData(nCols, i, holdData.slice(mapFromBoard(i,0,nCols),mapLastInRowFromBoard(i,nCols)+1))};
 		}
 		return tiles;
 	}
 
-	generateRowData(nCols, rowIndex, holdData){
+	const generateRowData = (nCols, rowIndex, holdData) =>{
 		let tempRow = [];
 		for (var j = 0; j < calcRowLen(nCols,rowIndex); j++) {
-			tempRow[j] = {ID: uuid(), holdData: {...holdData[j], ...{handleClick: this.holdHandleClick([rowIndex,j])}}, limbsData: {}};
+			tempRow[j] = {ID: uuid(), holdData: {...holdData[j], ...{handleClick: holdHandleClick([rowIndex,j])}}, limbsData: {}};
 		}
 		return tempRow;
 	}
 
-	mapLimbsToTiles(tiles){
-		Object.entries(this.state.limbData)
+	const mapLimbsToTiles = (tiles) => {
+		Object.entries(limbData)
 		.filter(([key,value]) => (!value.isAtStart))
 		.map(([key,value]) => {
 			let tempLimbsData = tiles[value.coords[0]].data[value.coords[1]].limbsData;
-			return (tiles[value.coords[0]].data[value.coords[1]].limbsData = {...tempLimbsData, ...this.formatLimbsForTile(key,value)});
+			return (tiles[value.coords[0]].data[value.coords[1]].limbsData = {...tempLimbsData, ...formatLimbsForTile(key,value)});
 		});
 		return (tiles);
 	}
 
-	getCardsToDisplay(nCardDraw){
-		const lastCardInDisplay = (this.state.drawIndex + nCardDraw) > this.state.drawPile.length ? this.state.drawPile.length : this.state.drawIndex + nCardDraw;
-		return this.state.drawPile.slice(this.state.drawIndex,lastCardInDisplay);
+	const getCardsToDisplay = (nCardDraw) => {
+		const lastCardInDisplay = (drawIndex + nCardDraw) > drawPile.length ? drawPile.length : drawIndex + nCardDraw;
+		return drawPile.slice(drawIndex,lastCardInDisplay);
 	}
 
-	generateMoveTree(){
-		const drawnCards = this.getCardsToDisplay(this.props.nCardDraw);
+	const generateMoveTree = () => {
+		const drawnCards = getCardsToDisplay(props.nCardDraw);
 
 		const topFirst = (card,remainingCards,recurs) => {
 			return {[card.data.colors[0]]:{[card.data.colors[1]]:recurs(remainingCards)}};
@@ -120,134 +116,126 @@ class Game extends React.Component{
 			},{});
 		}
 
-		console.log(generateMoveOptions(drawnCards));
+		return generateMoveOptions(drawnCards);
 
 	}
 
-	generateDrawPile(){
-		const drawPile = colorsArray.map((firstColor,index) => {
-			return (colorsArray.slice(index, colorsArray.length).map(secondColor => {
-				return ({ID: uuid(), data: {colors: [firstColor, secondColor]}})
-			}))
-		}).flat();
-		return drawPile;
-	}
+	
 
-	limbHandleClick(limb){
-		if(this.state.limbData[limb].selected){
-			this.deselectLimb(limb);
+	const limbHandleClick = (limb) => {
+		if(limbData[limb].selected){
+			deselectLimb(limb);
 		} else {
-			this.selectLimb(limb);
+			selectLimb(limb);
 		}
 	}
 
-	selectLimb = (limb) => {
-		const tempLimbState = {...this.state.limbData, ...{[limb] : {...this.state.limbData[limb], ...{selected:true}}}};
-		this.setState((state) => ({limbData: tempLimbState}));
+	const selectLimb = (limb) => {
+		const tempLimbState = {...limbData, ...{[limb] : {...limbData[limb], ...{selected:true}}}};
+		setLimbData(tempLimbState);
 		}
 
-	deselectLimb = (limb) => {
-		const tempLimbState = {...this.state.limbData, ...{[limb] : {...this.state.limbData[limb], ...{selected:false}}}};
-		this.setState((state) => ({limbData: tempLimbState}));
+	const deselectLimb = (limb) => {
+		const tempLimbState = {...limbData, ...{[limb] : {...limbData[limb], ...{selected:false}}}};
+		setLimbData(tempLimbState);
 	}
 
-	holdHandleClick = (coords) => () => {
-		let selectedLimbs = Object.entries(this.state.limbData).filter(([key,value]) => (value.selected)).map(([key,value]) => (key));
-		selectedLimbs = this.validMoves(selectedLimbs,coords);
-		this.moveLimbs(selectedLimbs,coords);
+	const holdHandleClick = (coords) => () => {
+		let selectedLimbs = Object.entries(limbData).filter(([key,value]) => (value.selected)).map(([key,value]) => (key));
+		selectedLimbs = validateMove(selectedLimbs,coords);
+		moveLimbs(selectedLimbs,coords);
 	} 
 
-	validMoves = (limbs,coords) => {
-		limbs = limbs.filter((limb) => (this.props.maxMoveDist >= dist(this.state.limbData[limb].coords,coords))); //max distance from ogigin
+	const validateMove = (limbs,coords) => {
+		limbs = limbs.filter((limb) => (props.maxMoveDist >= dist(limbData[limb].coords,coords))); //max distance from ogigin
 
 		limbs = limbs.filter(selectedLimb => //max distance between limbs
-			!this.state.limbData[selectedLimb].group.reduce((invalidGroup, groupee) => (
+			!limbData[selectedLimb].group.reduce((invalidGroup, groupee) => (
 				invalidGroup ||
-				Object.values(this.state.limbData)
+				Object.values(limbData)
 				.filter(limb => (limb.group.find(element => element === groupee)))
-				.reduce((invalid,limb) => (invalid || this.props.maxGroupDist < dist(limb.coords,coords)),false))
+				.reduce((invalid,limb) => (invalid || props.maxGroupDist < dist(limb.coords,coords)),false))
 				,false)
 			)
+
+
 		return limbs;
 	}
 
-	moveLimbs = (limbs, coords) => {
-		let tempLimbsState = {...this.state.limbData};
+	const moveLimbs = (limbs, coords) => {
+		let tempLimbsState = {...limbData};
 		limbs.forEach((limb) => {
-			tempLimbsState = {...tempLimbsState, ...{[limb] : {...this.state.limbData[limb], ...{coords: coords, isAtStart: false, selected: false}}}};
+			tempLimbsState = {...tempLimbsState, ...{[limb] : {...limbData[limb], ...{coords: coords, isAtStart: false, selected: false}}}};
 			})
-		this.setState((state) => ({limbData: tempLimbsState}));
+		setLimbData(tempLimbsState);
 		}
 
-	formatLimbsForTile(key,value){
-		return{[key]: {selected: value.selected, handleClick: this.limbHandleClick}};
+	const formatLimbsForTile = (key,value) => {
+		return{[key]: {selected: value.selected, handleClick: limbHandleClick}};
 	}
 
-	drawCards(){
-		const newDrawIndex = this.state.drawIndex + this.props.nCardDraw;
-		this.setState({drawIndex: newDrawIndex, 
-			displayDraw: newDrawIndex + this.props.nCardDraw < this.state.drawPile.length});
-		this.generateMoveTree();
+	const drawCards = () => {
+		const newDrawIndex = drawIndex + props.nCardDraw;
+		setDrawIndex(newDrawIndex);
+		setDisplayDraw(newDrawIndex + props.nCardDraw < drawPile.length);
+		generateMoveTree();
 
 	}
 
-	generateLimbsAtStart(){
+	const generateLimbsAtStart = () => {
 		let limbsAtStart = {};
-		Object.entries(this.state.limbData)
+		Object.entries(limbData)
 		.filter(([key,value]) =>(value.isAtStart))
 		.forEach(([key,value]) => (
-			limbsAtStart = {...limbsAtStart, ...this.formatLimbsForTile(key,value)}
+			limbsAtStart = {...limbsAtStart, ...formatLimbsForTile(key,value)}
 			));
 		return	limbsAtStart;
 	}
 
 
-
-	render(){
-
-		const tilesData = this.generateTilesData(this.props.nRows, this.props.nCols);
-		const cardDisplay = this.getCardsToDisplay(this.props.nCardDraw);
-		const limbsAtStart = this.generateLimbsAtStart();
+	const tilesData = generateTilesData(props.nRows, props.nCols);
+	const cardDisplay = getCardsToDisplay(props.nCardDraw);
+	const limbsAtStart = generateLimbsAtStart();
 	
-		return(
-			<>
-				<div>
-				{tilesData.reverse().map((row) => {
-					return(
-						<div key = {row.ID} className = "row">
-							{row.data.map((tile) => {
-								return (<Tile key = {tile.ID} limbsData = {tile.limbsData}>
-									<Hold holdData = {tile.holdData} key = {tile.ID} />
-									</Tile>)
-							})}
-						</div>
-					)	
-				})}
-				</div>
-				<div>
-					{limbsAtStart ? (<Tile limbsData = {limbsAtStart}>
-						<Hold/>
-					</Tile>) : null}
-				
-				</div>
-				<div className = "playerControls">
-					<div className ="cardDisplay">
-						{cardDisplay.map((card) => {
-							return(<Card key = {card.ID} data = {card.data}/>)
-						})
-						}
+	return(
+		<>
+			<div>
+			{tilesData.reverse().map((row) => {
+				return(
+					<div key = {row.ID} className = "row">
+						{row.data.map((tile) => {
+							return (<Tile key = {tile.ID} limbsData = {tile.limbsData}>
+								<Hold holdData = {tile.holdData} key = {tile.ID} />
+								</Tile>)
+						})}
 					</div>
-					{this.state.displayDraw ?
-						<button onClick = {this.drawCards}> 
-							Draw
-						</button>
-						: null}						
-				</div>	
-				
-			</>
+				)	
+			})}
+			</div>
+			<div>
+				{limbsAtStart ? (<Tile limbsData = {limbsAtStart}>
+					<Hold/>
+				</Tile>) : null}
+			
+			</div>
+			<div className = "playerControls">
+				<div className ="cardDisplay">
+					{cardDisplay.map((card) => {
+						return(<Card key = {card.ID} data = {card.data}/>)
+					})
+					}
+				</div>
+				{displayDraw ?
+					<button onClick = {drawCards}> 
+						Draw
+					</button>
+					: null}						
+			</div>	
+			
+		</>
 
-		)
-	}
+	)
+
 }
 
 
