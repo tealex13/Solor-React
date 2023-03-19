e617d3dc562a2c593a49f1f05161bc08ad0b0f57
Alex.Avila <alex.avila@sisudevices.com>
2023-03-18 21:12:49 -0500

    Feat: Change tile to a display: grid

--- src/Components/Tile.js
+++ src/Components/Tile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Limb} from "./Limb";
+import "./Tile.css"
 
 export const limbType = {
 	leftHand: "leftHand",
@@ -23,29 +24,28 @@ export function Tile(props){
 		}
 
 		return(
-			<div>
-				<div className = "limbRow topRow">
-					<div>
-						{props.limbsData[limbType.leftHand] ? 
-						<Limb handleClick = {handleClick(limbType.leftHand)} selected = {props.limbsData.leftHand.selected}/> : null}
-					</div>
-					<div>
-						{props.limbsData[limbType.rightHand] ? 
-						<Limb handleClick = {handleClick(limbType.rightHand)} selected = {props.limbsData.rightHand.selected}/> : null} 
-					</div>
-				</div>				
-					{React.cloneElement(props.children, {children: (props.limbsData[limbType.weight] ? 
+			<div className = "tile">
+				<div className = "central background">
+					{React.cloneElement(props.children, {className:"central", children: (props.limbsData[limbType.weight] ? 
 						<Limb handleClick = {handleClick(limbType.weight)} selected = {props.limbsData.weight.selected}/> : null)})}
-				 <div className = "limbRow bottomRow">
-					<div>
-						{props.limbsData[limbType.leftFoot] ? 
-						<Limb handleClick = {handleClick(limbType.leftFoot)} selected = {props.limbsData.leftFoot.selected}/> : null}
-					</div>
-					<div>
-						{props.limbsData[limbType.rightFoot] ? 
-						<Limb handleClick = {handleClick(limbType.rightFoot)} selected = {props.limbsData.rightFoot.selected}/> : null} 
-					</div>
 				</div>
+				<div className = "top left overlay">
+				{props.limbsData[limbType.leftHand] ? 
+						<Limb handleClick = {handleClick(limbType.leftHand)} selected = {props.limbsData.leftHand.selected}/> : null}
+				</div>
+				<div className = "top right overlay">
+				{props.limbsData[limbType.rightHand] ? 
+						<Limb handleClick = {handleClick(limbType.rightHand)} selected = {props.limbsData.rightHand.selected}/> : null} 
+				</div>
+				<div className = "bottom left overlay">
+				{props.limbsData[limbType.leftFoot] ? 
+						<Limb handleClick = {handleClick(limbType.leftFoot)} selected = {props.limbsData.leftFoot.selected}/> : null}
+				</div>
+				<div className = "bottom right overlay">
+				{props.limbsData[limbType.rightFoot] ? 
+						<Limb handleClick = {handleClick(limbType.rightFoot)} selected = {props.limbsData.rightFoot.selected}/> : null} 
+				</div>
+				
 			</div>
 
 		)
