aa4c91396149be4e5cc7d3868c314ec558e9906c
Alex.Avila <alex.avila@sisudevices.com>
2023-03-21 04:38:21 -0500

    Feat: Display arrows on cards

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,15 +2,21 @@ import React from 'react';
 import './Card.css';
 
 export function Card(props) {
+	const displayColor = (index) => {
+		return (<div  style={{background: props.data.colors[index]}} className = "cardColor">
+				</div>);
+	}
 	return(
 		<span className = "card">
-			{props.data.colors.map((color, index) => {
-				return(
-					<div  style={{background: color}} className = "cardColor">
-					</div>
-					)
-			})}
+
+			<>
+				{displayColor(0)}
+				<img src={require("./Arrow.png")} alt="right Arrow" className = {"arrow " + props.data.weightDir}/>
+				{displayColor(1)}
+			</>
 		</span>
 	)
 }
+//transform: scalex{-1};
+//classNamr ={${wieghtDir === dirs.left} ? ""} 
 
